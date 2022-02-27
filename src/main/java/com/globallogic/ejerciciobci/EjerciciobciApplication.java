12bbb7feac71fa8ba6d91eb44e6f79c9556a3bfe
unknown <julian.aga@globallogic.com>
2022-02-27 02:22:53 -0300

    configured gradle

---
+++ src/main/java/com/globallogic/ejerciciobci/EjerciciobciApplication.java
@@ -0,0 +1,13 @@
+package com.globallogic.ejerciciobci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciobciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciobciApplication.class, args);
+	}
+
+}
