145ee543a862ff8fc86182b46b161d901f9d204f
julian <julian.aga@globallogic.com>
2022-03-28 00:14:12 -0300

    configured h2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into phone(id,number,citycode,contrycode) values(123, 123123, 223, 54);
+insert into phone(id,number,citycode,contrycode) values(133, 43324, 223, 54);
+insert into phone(id,number,citycode,contrycode) values(153, 939393, 223, 54);
+insert into phone(id,number,citycode,contrycode) values(163, 432545, 223, 54);
